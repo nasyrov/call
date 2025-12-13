5f37bb26226293b2704752f19d235cbf1613f7cb
Evgenii Nasyrov <enasyrov@icloud.com>
2025-12-13 14:42:19 +0000

    feat: scaffold dashboard layout

---
+++ src/app/(dashboard)/_components/nav-main.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Calendar, History, Home, Video } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "~/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Upcoming",
+    url: "/upcoming",
+    icon: Calendar,
+  },
+  {
+    title: "Previous",
+    url: "/previous",
+    icon: History,
+  },
+  {
+    title: "Recordings",
+    url: "/recordings",
+    icon: Video,
+  },
+];
+
+export function NavMain() {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.title}>
+            <SidebarMenuButton
+              asChild
+              isActive={pathname === item.url}
+              tooltip={item.title}
+            >
+              <Link href={item.url}>
+                <item.icon />
+                <span>{item.title}</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
